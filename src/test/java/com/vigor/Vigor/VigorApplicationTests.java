43474d085d86dd3932d241c0aed05ebe20f451bf
Осинцев Павел <pavel.osincev04@mail.ru>
2024-07-24 22:04:43 +0500

    Initial commit

---
+++ src/test/java/com/vigor/Vigor/VigorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vigor.Vigor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VigorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
