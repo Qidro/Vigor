6c7e7f77bd831bc8655d4b42005c638bb2ba8fce
Осинцев Павел <pavel.osincev04@mail.ru>
2024-07-24 23:52:23 +0500

    Создание админ панели

--- src/main/java/com/vigor/Vigor/controllers/MainController.java
+++ src/main/java/com/vigor/Vigor/controllers/MainController.java
@@ -8,10 +8,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+    //переадресация на главную страницу
     @GetMapping("/")
     public String Vigor( Model model) {
         model.addAttribute("title", "Vigor - катера");
         return "Vigor-get";
+    }
+
+    //переадресация на страницу "Приемущество"
+    @GetMapping("/Advantages")
+    public String Advantages( Model model) {
+        model.addAttribute("title", "Vigor - катера");
+        return "Advantages";
+    }
+
+    //переадресация на админ панель сайта
+    @GetMapping("/admin")
+    public String Admin_panel( Model model) {
+        model.addAttribute("title", "Админ панель сайта");
+        return "Admin";
     }
 
 }
